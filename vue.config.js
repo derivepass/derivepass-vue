89f6cfac46c8b82fbca5aa9385ba7c7cedf64647
Fedor Indutny <fedor@indutny.com>
2019-01-10 23:30:47 +0100

    pwa: fix over-caching

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,10 @@ module.exports = {
   pwa: {
     name: 'DerivePass',
     themeColor: '#3D79DE',
-    msTileColor: '#3d79de'
+    msTileColor: '#3d79de',
+    workboxOptions: {
+      skipWaiting: true,
+      clientsClaim: true,
+    },
   }
 }
