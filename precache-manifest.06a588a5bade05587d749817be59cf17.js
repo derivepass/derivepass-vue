f9413c24c12e68cd1ad06033dfff1aad29dcb9ce
Fedor Indutny <fedor@indutny.com>
2019-01-11 05:03:40 +0100

    fcda02e

---
+++ precache-manifest.06a588a5bade05587d749817be59cf17.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "42274085fdf6d9b7f4f1",
+    "url": "/css/app.865af462.css"
+  },
+  {
+    "revision": "42274085fdf6d9b7f4f1",
+    "url": "/js/app.53686da7.js"
+  },
+  {
+    "revision": "bca1c7e0ae52f40a7efd",
+    "url": "/css/chunk-vendors.3ce0e8f7.css"
+  },
+  {
+    "revision": "bca1c7e0ae52f40a7efd",
+    "url": "/js/chunk-vendors.e0713af0.js"
+  },
+  {
+    "revision": "84b1d3da7039647d7d14d49f18a9a538",
+    "url": "/749afdcf43048d4d5ed7.worker.js"
+  },
+  {
+    "revision": "92f3740a979c002cd3cfae49ac4c0586",
+    "url": "/index.html"
+  },
+  {
+    "revision": "08348bfb2d9c2d71bd6334bc45108c03",
+    "url": "/favicon.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
