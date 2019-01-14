6ee3c21692320e7583c59e82d93fb396b9bcd77f
Fedor Indutny <fedor@indutny.com>
2019-01-14 13:31:03 +0100

    application: do not throw on invalid input

--- src/pages/application.vue
+++ src/pages/application.vue
@@ -312,7 +312,7 @@ export default {
     invalidAllowedFeedback() {
       const allowed = this.app.options.allowed;
       if (allowed.length === 0) {
-        throw new Error('Can\'t be empty');
+        return 'Can\'t be empty';
       }
 
       try {
