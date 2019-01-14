5130acf6ac844319b765be87de6998eba3089991
Fedor Indutny <fedor@indutny.com>
2019-01-14 15:00:00 +0100

    application: add danger paragraph

--- src/pages/application.vue
+++ src/pages/application.vue
@@ -50,6 +50,11 @@
           <p>
             <b>Recommended</b> configuration is available for this domain.
           </p>
+          <p class="text-danger" v-if="!isNew">
+            Changing configuration for an existing app will change computed
+            computed password. Consider computing and copying current password
+            <b>before</b> using making a change.
+          </p>
           <b-button variant="warning" @click.prevent="usePreset">Use</b-button>
         </b-alert>
 
