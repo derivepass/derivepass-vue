a78e5189b6338da8735b640dbca6e0b4fb8f6f23
Fedor Indutny <fedor@indutny.com>
2019-01-21 00:43:53 +0100

    src: hex cloudkit out

--- src/pages/settings.vue
+++ src/pages/settings.vue
@@ -1,17 +1,15 @@
 <template>
   <div class="text-center">
-    <cloud-kit class="mb-2"/>
     <qr/>
   </div>
 </template>
 
 <script>
-import CloudKit from '../components/cloud-kit.vue';
 import Qr from '../components/qr.vue';
 
 export default {
   name: 'settings',
-  components: { CloudKit, Qr },
+  components: { Qr },
 };
 </script>
 
--- src/plugins/sync/index.js
+++ src/plugins/sync/index.js
@@ -1,13 +1,8 @@
-// Immediately start connecting to iCloud
-import CloudKit from '../../utils/sync/cloud-kit';
 import Local from '../../utils/sync/local-storage';
 
 // Enable cloud sync
 export default {
   install(Vue, options) {
-    const cloudKit = new CloudKit(options.store);
-    Vue.prototype.$cloudKit = cloudKit;
-
     // Enable local sync
     const local = new Local(options.store);
     Vue.prototype.$localStorage = local;
