2ff633bd57cc4896c280c5a943f41c3b86811ba0
Fedor Indutny <fedor@indutny.com>
2019-04-09 18:17:17 -0400

    public: apply CSP only for production builds

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
+const CspHtmlWebpackPlugin = require('csp-html-webpack-plugin');
+
 module.exports = {
   pwa: {
     name: 'DerivePass',
@@ -17,5 +19,32 @@ module.exports = {
       localeDir: 'locales',
       enableInSFC: true
     }
-  }
+  },
+
+  configureWebpack: {
+    plugins: [
+      new CspHtmlWebpackPlugin({
+        'default-src': '\'self\'',
+        'script-src': '\'self\'',
+        'img-src': [ '\'self\'', 'data:' ],
+        'style-src': [ '\'self\'', '\'unsafe-inline\'' ],
+        'connect-src': [ '\'self\'', 'data:', 'https://api.apple-cloudkit.com' ],
+      }, {
+        enabled: () => {
+          // Dev builds should be possible to run locally!
+          return process.env.NODE_ENV !== 'development';
+        },
+
+        // TODO(indutny): reconsider those
+        hashEnabled: {
+          'script-src': false,
+          'style-src': false,
+        },
+        nonceEnabled: {
+          'script-src': false,
+          'style-src': false,
+        },
+      }),
+    ],
+  },
 }
